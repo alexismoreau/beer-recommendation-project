664f653d9a5a1ebf9abe00d96b1616b6e3d77946
Alexis <alexismoreau2@live.fr>
2017-01-14 23:45:20 +0100

    add ex3

--- script.R
+++ script.R
@@ -97,3 +97,21 @@ stat.comp <- function(x,y){
 print(sapply(d,stat.comp,y=groupes.cah))
 
 # Groups are mostly dominated by review_aroma, review_palate and review_appearance.
+
+####################### Question 3 #######################
+
+library(recommenderlab)
+library(data.table)
+
+beerdata = data.table::fread("beer_reviews.csv")
+
+# Loading to pre-computed affinity data	
+affinity.data<-beerdata[,c(4,11)]
+affinity.matrix<- as(affinity.data,"realRatingMatrix")
+
+# Creation of the model - U(ser) B(ased) C(ollaborative) F(iltering)
+Rec.model<-Recommender(affinity.matrix, method = "UBCF")
+
+recommended.items.1 <- predict(Rec.model, affinity.matrix["1",], n=10)
+
+as(recommended.items.1, "list")
